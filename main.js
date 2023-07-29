b8316b219975b459dab1bc02869a4d414ab44d16
risaltte <risaltte@gmail.com>
2023-07-29 09:20:31 -0400

    Tooltips notifications header

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 import Alpine from 'alpinejs';
+import tippy from 'tippy.js';
+
+// CSS TippyJS
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
  
 window.Alpine = Alpine; 
 Alpine.start();
+
+window.tippy = tippy;
