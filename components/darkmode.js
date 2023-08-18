2826e3672f4ce87291c618349073e7467b92b613
risaltte <risaltte@gmail.com>
2023-08-18 13:40:09 -0400

    Cookie strategy persist

--- components/darkmode.js
+++ components/darkmode.js
@@ -1,9 +1,13 @@
+import Cookies from "js-cookie";
+
 export function darkmode() {
     return {
-        darkmodeOn: false,
+        darkmodeOn: JSON.parse(Cookies.get('darkmodeOn') ?? false),
 
         toggleDarkmode() {
             this.darkmodeOn = !this.darkmodeOn
+            
+            Cookies.set('darkmodeOn', JSON.stringify(this.darkmodeOn));
         }
     };
 }
