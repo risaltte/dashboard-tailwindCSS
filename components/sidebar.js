888af91eab92a5b27466fca13b2f504f177b2bf2
risaltte <risaltte@gmail.com>
2023-08-19 17:55:08 -0400

    Fix dashboard

--- components/sidebar.js
+++ components/sidebar.js
@@ -1,11 +1,24 @@
 export function sidebar() {
     return {
-        openSidebar: false,
+        MediumScreen: 768,
+        openSidebar: window.innerWidth >= this.MediumScreen,
         openUserPreferencesSidebar: false,
 
         toggleSidebar() {
             this.openSidebar = !this.openSidebar;
-            this.openUserPreferencesSidebar = false
+            this.openUserPreferencesSidebar = false;
+
+            let isMediumScreen = window.innerWidth >= this.MediumScreen;
+
+            if (!isMediumScreen) {
+                this.$refs.sidebar.classList.toggle('-ml-[500px]');
+
+                return;
+            }
+
+            this.$refs.sidebar.classList.toggle('md:ml-0');
+            this.$refs.main.classList.toggle('md:ml-64');
+            this.$refs.header.classList.toggle('md:ml-64');  
         },
 
         toggleOpenUserPreferencesSidebar() {
