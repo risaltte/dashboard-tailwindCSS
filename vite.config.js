206208badcd7367fcdc31a42189a5be02636265f
risaltte <risaltte@gmail.com>
2023-08-22 10:52:21 -0400

    Refactory

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,21 @@
+import {resolve} from 'path';
+
 export default {
     server: {
         host: '0.0.0.0'
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                login: resolve(__dirname, 'pages/login.html'),
+                register: resolve(__dirname, 'pages/register.html'),
+                recovery_password: resolve(__dirname, 'pages/recovery_password.html'),
+                courses_index: resolve(__dirname, 'pages/courses_index.html'),
+                courses_view: resolve(__dirname, 'pages/courses_view.html'),
+                courses_add: resolve(__dirname, 'pages/courses_add.html'),
+                courses_edit: resolve(__dirname, 'pages/courses_edit.html'),
+            }
+        }
     }
 }
